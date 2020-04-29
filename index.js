d4adecd068f887a94eca36987aa074afb3d61d28
GeorgeCoops <coopsgeorge@gmail.com>
2020-04-29 18:13:03 +0100

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 // Add your functions here
-function map(src, cb) {
-    let r = []
-  
-    for (let i = 0; i < src.length; i++) {
-      let theElement = src[i]
-      r.push(cb(theElement))
+const map = (array, functionA) => {
+    const newArray = []
+    for(let i = 0; i < array.length; i++){
+        newArray.push(functionA(array[i]))
     }
-  
-    return r;
-  }
-  
-  function reduce(src, cb, starting){
+    return newArray
+}
+/////// what the fuck is that hahaha//////
+function reduce(src, cb, starting){
     let r = (!!starting) ? starting : src[0]
     let i = (!!starting) ? 0 : 1
   
