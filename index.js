416660e7d7634bb7fa294ef4dbfdf21860fd1504
BenDunjay <bendunjay@outlook.com>
2020-05-03 16:10:55 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+const map = (array, whatToDo) => {
+    return array.map(number => whatToDo(number))
+}
+
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
