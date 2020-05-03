2d5bf63c674534f9b1fd96cab22fe4fa5b2cd736
BenDunjay <bendunjay@outlook.com>
2020-05-03 16:12:56 +0100

    Done.

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const map = (array, whatToDo) => {
     return array.map(number => whatToDo(number))
 }
 
+const reduce = (array, startingValue = 0) => {
+    return array.reduce(function(memo, i) {memo + i }, startingValue)
+}
 
-function reduce(src, cb, starting){
-    let r = (!!starting) ? starting : src[0]
-    let i = (!!starting) ? 0 : 1
+
+
+// function reduce(src, cb, starting){
+//     let r = (!!starting) ? starting : src[0]
+//     let i = (!!starting) ? 0 : 1
   
-    for (; i < src.length; i++) {
-      r = cb(src[i], r)
-    }
+//     for (; i < src.length; i++) {
+//       r = cb(src[i], r)
+//     }
   
-    return r;
-  }
+//     return r;
+//   }
